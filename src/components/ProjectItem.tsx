b11b38df45d6f3ad108296634410876cafd761d8
Anubis <anubis356@outlook.com>
2024-11-27 13:31:33 +0200

    fix: removing projects for now

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,41 +1,41 @@
 // import { Button } from "./ui/button";
-// import {
-//   Card,
-//   CardHeader,
-//   CardTitle,
-//   CardDescription,
-//   CardContent,
-//   CardFooter,
-// } from "./ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./ui/card";
 
-// type ProjectProps = {
-//   image: string;
-//   name: string;
-//   description: string;
-//   stack: string[];
-//   code: string;
-//   live: string;
-// };
-// export default function ProjectItem({
-//   image,
-//   name,
-//   description,
-//   stack,
-//   code,
-//   live,
-// }: ProjectProps) {
-//   return (
-//     <Card>
-//       <CardHeader>
-//         <CardTitle>{name}</CardTitle>
-//         <CardDescription>{description}</CardDescription>
-//       </CardHeader>
-//       <CardContent>
-//         <p>Card Content</p>
-//       </CardContent>
-//       <CardFooter>
-//         <p>Card Footer</p>
-//       </CardFooter>
-//     </Card>
-//   );
-// }
+type ProjectProps = {
+  image: string;
+  name: string;
+  description: string;
+  stack: string[];
+  code: string;
+  live: string;
+};
+export default function ProjectItem({
+  // image,
+  name,
+  description,
+}: // stack,
+// code,
+// live,
+ProjectProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+}
